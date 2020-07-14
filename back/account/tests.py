182b7f5098854626493bb9cb01bb07df496fded2
Ali Najafi <alinajafilerning@gmail.com>
2020-07-14 13:03:57 +0430

    simple testing methods of transaction and account added

--- back/account/tests.py
+++ back/account/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from . import models
 from django.contrib.auth.models import User
+from django.db.models import Sum,Q
 class GeneralTest(TestCase):
 
     def setUp(self):
@@ -10,26 +11,6 @@ class GeneralTest(TestCase):
         first_name= 'Ali',
         last_name = 'Najafi'
         )
-        self.acc1 = models.Account.objects.create(
-            user=self.u1,
-            name='my_acc1',
-            total=1000
-        )
-        self.test_trans = models.Transaction.objects.create(
-        amount = 200,
-        account=self.acc1
-        )
-
-        self.test_cat = models.Category.objects.create(
-        name='cat1'
-        )
-
-        self.test_tag = models.Tag.objects.create(
-        name='tag1'
-        )
-
-    def tearDown(self):
-        self.acc1.total = 1000
 
     def create_cat(self,num):
         lst =[]
@@ -40,23 +21,119 @@ class GeneralTest(TestCase):
             lst.append(obj)
         return lst
 
+    def create_tag(self,num,transactions=0):
+        lst=[]
+        for i in range(num):
+            obj = models.Tag.objects.create(
+            name=f'tag{1}'
+            )
+            lst.append(obj)
+        return lst
 
-    def test_transaction_cost(self):
-        trans1 = models.Transaction.objects.create(
+    def create_trans(self,num,account=None,amount=100,is_expense=True,tag=0,cat=0):
+        lst=[]
+        if not account:
+            account = self.acc1
+        for i in range(num):
+            obj = models.Transaction.objects.create(
+            account = account,
+            amount = amount,
+            note=f'note{i}',
+            is_expense=is_expense
+            )
+
+            if tag:
+                instances = self.create_tag(tag)
+                obj.tag.add(*instances)
+            if cat:
+                if cat >=1:
+                    category = self.create_cat(1)[0]
+                    obj.category = category
+            lst.append(obj)
+        return  lst
+
+    def create_account(self,num,total=1000,balance=0):
+        lst=[]
+        for i in range(num):
+            obj = models.Account.objects.create(
+            name=f'acc{i}',
+            user=self.u1,
+            total=total,
+            balance=balance
+            )
+            lst.append(obj)
+
+        return lst
+
+    def create_user(self,num):
+        lst = []
+        for i in range(num):
+            obj = User.objects.create_user(
+            username=f'username_{1}',
+            first_name = f'first_{1}',
+            last_name = f'last_{1}',
+            email=f'email_{1}@email.com'
+            )
+            lst.append(obj)
+        return lst
+
+    def test_transaction(self):
+        ac1 = models.Account.objects.create(
+        user=self.u1,
+        name='acc2',
+        total=1000
+        )
+        t1 = models.Transaction(
+        account = ac1,
+        amount = 100,
+        note='note1'
+        )
+        t2 = models.Transaction(
+        account = ac1,
         amount = 200,
-        account=self.acc1
+        note='note2',
+        is_expense=False
         )
 
-        self.acc1.add_transaction(trans1)
-        self.assertEqual(self.acc1.total,800)
+        self.assertEqual(ac1.total,1000)
+        self.assertEqual(ac1.balance,0)
 
-    def test_transaction_income(self):
-        trans1 = models.Transaction.objects.create(
-        amount = 200,
-        account=self.acc1
+        self.assertEqual(t1.amount,100)
+        self.assertTrue(t1.is_expense,True)
+
+        self.assertIsNone(t1.category)
+        self.assertEqual(t1.__str__(),'-100 from acc2')
+
+        self.assertEqual(t2.__str__(),'+200 from acc2')
+
+        t1.save()
+        self.assertEqual(ac1.total,900)
+        self.assertIn(t1,ac1.transaction_set.all())
+
+        t2.save()
+        self.assertEqual(ac1.total,1100)
+        self.assertIn(t1,ac1.transaction_set.all())
+
+    def test_account(self):
+        #before transactions
+        acc1 = self.create_account(1)[0]
+        self.assertEqual(acc1.total,1000)
+        self.assertEqual(acc1.balance,0)
+        #after transactions
+        expss = self.create_trans(1,acc1,100)
+        incs = self.create_trans(2,acc1,200,False)
+
+
+        self.assertEqual(acc1.total,1300)
+
+        self.assertEqual(
+        acc1.transaction_set.filter(is_expense=True).count(),
+        len(expss)
+        )
+        self.assertEqual(
+        acc1.transaction_set.filter(is_expense=False).count(),
+        len(incs)
         )
 
-        self.acc1.add_transaction(trans1)
-        self.assertEqual(self.acc1.total,1200)
-
-    
+        acc1.update_balance()
+        self.assertEqual(acc1.balance,300)
