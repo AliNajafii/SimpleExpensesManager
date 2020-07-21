08fae958dccffbca5ebdd4ca724ec490875a0b0b
Ali Najafi <alinajafilerning@gmail.com>
2020-07-21 11:14:30 +0430

    account listing and creating added and account profile some changes

--- back/account/account_views.py
+++ back/account/account_views.py
@@ -1,7 +1,5 @@
 from rest_framework.views import APIView
 from rest_framework import generics
-from rest_framework.exceptions import MethodNotAllowed
-from rest_framework import exceptions
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import get_user_model
@@ -10,22 +8,46 @@ from rest_framework.response import Response
 from rest_framework import status
 from . import serialization
 from rest_framework.renderers import JSONRenderer
-from rest_framework.decorators import renderer_classes
+from . import serialization
+from . import models
+
 
 class AccountCreateView(generics.CreateAPIView):
-    pass
+    serializer_class = serialization.AccountSerializer
+    model = models.Account
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JSONWebTokenAuthentication,)
+
+
+
+    def post(self,request,*args,**kwargs):
+
+        seri = serialization.AccountSerializer(data=request.data,
+        context={'user':request.user})
+        if seri.is_valid():
+            seri.save()
+            account = seri.instance
+            request.user.account_set.add(account)
+            return Response(
+            data=seri.data,
+            status= status.HTTP_201_CREATED
+            )
+        return Response(
+        data= seri.errors,
+        status= status.HTTP_400_BAD_REQUEST
+        )
 
 
 class AccountProfileView(APIView):
-    # permission_classes = (IsAuthenticated,)
-    # authentication_classes = (JSONWebTokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JSONWebTokenAuthentication,)
     USER = get_user_model()
     renderer_classes = (JSONRenderer,)
     def get(self,request,*args,**kwargs):
         try :
-            user = self.USER.objects.get(username = kwargs.get('username'))
+            user = request.user
             account = user.account_set.get(name = kwargs.get('account_name'))
-            seri = serialization.AccountSerializer(account)
+            seri = serialization.AccountSerializer(account,fields=('balance','transaction_set',),context={'transaction_fields':('amount',)})
             res = Response(data= seri.data,status=status.HTTP_200_OK)
 
             return res
@@ -39,9 +61,47 @@ class AccountProfileView(APIView):
         if not request.method == 'GET':
             raise MethodNotAllowed(method=request.method)
 
+class queryparams(APIView):
+    def get(self,request,*args,**kwargs):
+        return Response(
+        {
+        'params': request.query_params
+        }
+        )
+class UserAccountListView(generics.ListAPIView):
 
-class AccountListView(generics.ListAPIView):
-    pass
+    authentication_classes = (JSONWebTokenAuthentication,)
+
+    def get_queryset(self,*args,**kwargs):
+        return self.request.user.account_set.all()
+
+    def get_serializer(self,*args,**kwargs):
+        # client can choose the field for json representation
+        query_fields = self.request.query_params.get('fields')
+
+        seri = None
+        query = self.get_queryset()
+        context = {}
+
+        if query_fields:
+            query_fields = f',{query_fields}' # for avoiding none functionality
+            context['fields'] = query_fields.split(',')
+
+
+            seri = serialization.AccountSerializer(
+            query,
+            many=True,
+            **context
+            )
+
+            return seri
+
+        seri = serialization.AccountSerializer(
+        query,
+        many=True
+        )
+
+        return seri
 
 class AccountUpdateView(generics.UpdateAPIView):
     pass
