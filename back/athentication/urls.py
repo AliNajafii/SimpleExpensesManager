efc407d5e72e2b6119b62ec98aae88c944dd277b
AliNajafii <58827763+AliNajafii@users.noreply.github.com>
2020-07-23 11:04:38 +0430

    jwt auth removed

--- back/athentication/urls.py
+++ back/athentication/urls.py
@@ -1,9 +1,11 @@
 from rest_framework_jwt import views
 from django.urls import include,path
-
+from . import views as auth_view
 urlpatterns = [
 
-    path('login',views.obtain_jwt_token),
-    path('refresh-token',views.refresh_jwt_token),
+    # path('login',auth_view.JWTLoginView.as_view()),
+    # path('refresh-token',views.refresh_jwt_token),
+    # path('logout',auth_view.JWTLogoutView.as_view()),
+
 
 ]
