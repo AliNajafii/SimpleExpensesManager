d377bd2eaab9a4fe89e046eb4ef2dcc95453af99
AliNajafii <58827763+AliNajafii@users.noreply.github.com>
2020-07-25 15:56:55 +0430

    Update cat_urls.py

--- back/account/cat_urls.py
+++ back/account/cat_urls.py
@@ -3,10 +3,8 @@ from . import cat_views
 
 urlpatterns = [
 
-    path('<str:cat_name>',cat_views.CategoryDetailView.as_view(),name='cat-detail'),
+    path('<str:cat_name>/',cat_views.CategoryDetailView.as_view(),name='cat-detail'),
     path('lists',cat_views.CategoryListView.as_view()),
     path('create',cat_views.CategoryCreateView.as_view()),
-    path('delete',cat_views.CategoryDeleteView.as_view()),
-
 
 ]
