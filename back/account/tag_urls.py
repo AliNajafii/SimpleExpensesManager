cc98b9e80b5bfd0eea321b0c71593dcfd96dae84
AliNajafii <58827763+AliNajafii@users.noreply.github.com>
2020-07-25 13:31:34 +0430

    Update tag_urls.py

--- back/account/tag_urls.py
+++ back/account/tag_urls.py
@@ -3,10 +3,8 @@ from . import tag_views
 
 urlpatterns = [
 
-    path('<str:tag_name>',tag_views.TagDetailView.as_view(),name='tag-detail'),
+    path('<str:tag_name>/',tag_views.TagDetailView.as_view(),name='tag-detail'),
     path('lists',tag_views.TagListView.as_view()),
     path('create',tag_views.TagCreateView.as_view()),
-    path('delete',tag_views.TagDeleteView.as_view()),
-
 
 ]
