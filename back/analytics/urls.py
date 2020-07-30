a86696347e2207cf733a7e618c8fe3ddf262de1f
AliNajafii <58827763+AliNajafii@users.noreply.github.com>
2020-07-30 18:31:28 +0430

    router added

--- back/analytics/urls.py
+++ back/analytics/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
+from . import views
 
+router = DefaultRouter()
+router.register('accounts',views.AccountViewSet,basename='account-info')
 
 
 urlpatterns = []
+urlpatterns += router.urls
