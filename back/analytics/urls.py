1674c7d5252269e7709d514fa1884b614a75b350
AliNajafii <58827763+AliNajafii@users.noreply.github.com>
2020-07-31 12:49:28 +0430

    tag view set router added

--- back/analytics/urls.py
+++ back/analytics/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 router = DefaultRouter()
 router.register('accounts',views.AccountViewSet,basename='account-info')
-
+router.register('tags',views.TagInfoViewSet,basename='tag-info')
 
 urlpatterns = []
 urlpatterns += router.urls
