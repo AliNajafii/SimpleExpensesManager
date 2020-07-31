4274fcfcf78a33984a1cee792f0c183b91b4d5e2
AliNajafii <58827763+AliNajafii@users.noreply.github.com>
2020-07-31 12:49:08 +0430

    tag info serializer added

--- back/analytics/serializer.py
+++ back/analytics/serializer.py
@@ -79,3 +79,54 @@ class AccountInfoSerializer(DynamicFieldsModelSerializer):
 
         avg = obj.last_time_expense_avg(**time)
         return avg
+
+
+
+
+
+class TagInfoSerializer(DynamicFieldsModelSerializer):
+
+    transaction_number = serializers.SerializerMethodField(
+    'get_transactions_num',
+    read_only = True
+    )
+
+    expense = serializers.SerializerMethodField(
+    'get_expense_transactions',
+    read_only = True
+    )
+
+    income = serializers.SerializerMethodField(
+    'get_income_transactions',
+    read_only = True
+    )
+
+    balance = serializers.SerializerMethodField(
+    'get_transaction_balance',
+    read_only = True
+    )
+
+    class Meta:
+        model = account_models.Tag
+        fields = [
+        'balance','income','expense',
+        'transaction_number'
+        ]
+
+    def get_transactions_num(self,obj):
+        return obj.get_transactions_num()
+
+
+    def get_expense_transactions(self,obj):
+        queryset = obj.get_expense_transactions()
+        seri = TransactionSerializer(queryset,many=True)
+        return seri.data
+
+
+    def get_income_transactions(self,obj):
+        queryset = obj.get_income_transactions()
+        seri = TransactionSerializer(queryset,many=True)
+        return seri.data
+
+    def get_transaction_balance(self,obj):
+        return obj.get_transaction_balance()['balance']
