6e3931ca28e6376787d2e8f04d8094deb0aab5a5
AliNajafii <58827763+AliNajafii@users.noreply.github.com>
2020-08-04 13:52:30 +0430

    token login and logout added

--- back/athentication/urls.py
+++ back/athentication/urls.py
@@ -1,11 +1,10 @@
 from rest_framework_jwt import views
 from django.urls import include,path
 from . import views as auth_view
+from djoser.views import TokenCreateView,TokenDestroyView
+from .views import TokenLogout
 urlpatterns = [
-
-    # path('login',auth_view.JWTLoginView.as_view()),
-    # path('refresh-token',views.refresh_jwt_token),
-    # path('logout',auth_view.JWTLogoutView.as_view()),
-
+    path('token/login/',TokenCreateView.as_view()),
+    path('token/logout/',TokenLogout.as_view())
 
 ]
