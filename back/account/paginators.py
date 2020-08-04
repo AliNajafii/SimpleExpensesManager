372a03b3362941b6a52575a4061ec774cbe781e5
AliNajafii <58827763+AliNajafii@users.noreply.github.com>
2020-08-04 13:51:43 +0430

    Update paginators.py

--- back/account/paginators.py
+++ back/account/paginators.py
@@ -5,4 +5,4 @@ class DefaultPagination(PageNumberPagination):
     page_size = 10
     page_query_param = 'page'
     page_size_query_param = 'page_size'
-    max_page_size = 1000
+    max_page_size = 100
